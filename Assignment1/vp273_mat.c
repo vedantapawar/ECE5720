52ee44f49c94310ecd802e03d6e3086f6ced7c40
Vedanta Pralhad Pawar <vp273@en-ec-ecelinux-03.coecis.cornell.edu>
2020-02-04 22:41:12 -0500

    With Comments

--- Assignment1/vp273_mat.c
+++ Assignment1/vp273_mat.c
@@ -1,7 +1,12 @@
 /*
 Author: Vedanta Pawar
+NetID: vp273
 Class: M.Eng ECE, Cornell University
 Email: vp273@cornell.edu
+
+Instructions for Compiling and Executing Code:
+Compile: gcc vp273_mat.c -std=gnu99 -o vp273_mat
+Run: ./vp273_mat
 */
 
 #include <stdio.h>
@@ -9,46 +14,60 @@ Email: vp273@cornell.edu
 #include <stdlib.h>
 #include <time.h>
 
-#define MAX_LENGTH 8388608
-#define STRIDE 1
-#define MIN_SIZE 1024
-#define BILLION 1000000000L
-#define REPEAT 10
+#define MAX_LENGTH 1048576  // MAX_LENGTH is 2^20
+#define STRIDE 1            // Stride length 
+#define MIN_SIZE 1024       // MIN_LENGTH is 2^10
+#define BILLION 1000000000L // To convert clock time in floating point seconds to nano seconds
+#define REPEAT 10           // For averaging out the result over 10 trials
 
 int main( int argc, char *argv[] )
 {
-    uint64_t diff;
-	struct timespec start, end;
-    float* A;
-    FILE *ptr_file ;
+    float* A;                   // Pointer to the array
+    uint64_t diff;              // Stores the time in nanoseconds
+	struct timespec start, end; // Used to implement the high resolution timer included in <time.h>
+    double avg_time;            // Store the average time for a single access
+    FILE *ptr_file ;            // File pointer used to point to a .csv file for storing the time vs stride
 
+    /* Malloc an array of size MAX_LENGTH x size of float and 
+    assign the pointer to A */
     A = ( float* )malloc( MAX_LENGTH * sizeof( float ) );
-    if ( A == NULL )
+
+    if ( A == NULL ) // If memory could no be allocated then throw error message
     {
         printf( "Memory not allocated.\n" ); 
         exit(0); 
     }
     
-    ptr_file = fopen("output.csv", "w");
+    ptr_file = fopen("output.csv", "w");  // Save the time and corresponding stride in a csv file
+
+    // Iterate through the different array length. n:=2*n
     for ( int n = MIN_SIZE; n <= MAX_LENGTH ; n = 2 * n )
     {
+        // Iterate through different stride length. s:=2*n
     	for ( int s = 1 ; s <= n / 2 ; s = 2 * s )
     	{
+            // Start high resolution clock timer
     		clock_gettime( CLOCK_MONOTONIC , &start );
+            //Repeat the array access REPEAT x Stride times
     		for ( int j = 0 ; j < REPEAT * s ; j++ )
     		{
+                // Access("touch") the array n / s times
     			for ( int i ; i <= (int)( n / s ) ; i++ )
 	    		{
-	    			A[ i * s ] = 3.142857 ;
+	    			A[ i * s ] = 3.142857 ; // Touch the array element by storing pi
 	    		}
-    		}
-    		clock_gettime( CLOCK_MONOTONIC , &end );
+    		}            
+    		clock_gettime( CLOCK_MONOTONIC , &end );    // End clock timer.
+            //Calculate the difference in timer and convert it to nanosecond by multiplying by 10^9
     		diff = ( BILLION * ( end.tv_sec - start.tv_sec ) + end.tv_nsec - start.tv_nsec ) ;
-			printf( "elapsed time = %lf nanoseconds for STRIDE= %d and LENGTH= %d\n", ( double ) diff / ( n * REPEAT) , s , n);
-            fprintf( ptr_file ,"%d , %lf\n", s ,  ( double ) diff / ( n * REPEAT ) );
+            // Calculate average time for a single access by dividing by n x REPEAT
+            avg_time = ( double )diff / ( n * REPEAT ) ;
+			printf( "elapsed time = %lf nanoseconds for STRIDE= %d and LENGTH= %d\n", avg_time , s , n);
+            // Store the access time and corresponding stride length in a csv file
+            fprintf( ptr_file ,"%d , %lf\n", s ,  avg_time );
     	}
     }   
-    free ( A ) ;
-    fclose ( ptr_file ) ;
+    free ( A ) ;            // Free allocated memory pointed by A
+    fclose ( ptr_file ) ;   // Close the .csv file
     exit( 0 ) ;
 }
