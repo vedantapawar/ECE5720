8f93a34b066bf4a36a97875a8724b6e279704bda
vedantapawar <vedantapawar@gmail.com>
2020-02-12 21:53:28 -0500

    Sequential Without Pthread

--- Assignment2/vp273_hw2_code.c
+++ Assignment2/vp273_hw2_code.c
@@ -22,8 +22,11 @@ int main( int argc, char *argv[] )
 	struct timespec start, end; // Used to implement the high resolution timer included in <time.h>
 	int ndim ;					// Ask user and store the dimension of the square matrix
 	float sum = 0.0;			// Intermediate sum of the innerproduct of the row vector and column vector
+	float p;
+	int num;
+	float x ;
 
-	printf( "Enter the dimension of the matrix to be multiplied:\n" );
+	printf( "Enter the dimension of the matrix:\n" );
 	scanf("%d" , &ndim); 	// Store matrix dimension in ndim 	
 
 	/*
@@ -32,25 +35,71 @@ int main( int argc, char *argv[] )
 	The matrix is a linear array of size ndim x ndim
 	Total memory malloced is ndim^2
 	 */
-	double *matA = ( double * )malloc( ndim * ndim * sizeof( double ) );
-	
-	srand48 (1); 	// Set random seed to for initializing drand48() later
-
-	// Iterate through the rows of the Matrix A and B
+	double *matA = ( double * )malloc( ndim * ndim * sizeof( double ) ) ;
+	double *matB = ( double * )malloc( ndim * sizeof( double ) ) ;
+	 
+	// Iterate through the rows of the Matrix A 
 	for ( int i = 0 ; i < ndim ; i++ )
 	{
-		// Iterate through the columns of the Matrix A and B
+		// Iterate through the columns of the Matrix A 
 		for ( int j = 0 ; j < ndim ; j++ )
 		{
-			// Store same random numbers in A and B
-			*( matA + i * ndim + j ) = drand48() ;
+			srand48( clock() ) ; // Set random seed to for initializing drand48() later
+			// Store same random numbers in A 
+			*( matA + i * ndim + j ) = drand48() ;		
+			scanf( "%d" , &num )	;
+			*( matA + i * ndim + j ) = num ;		
 		}
+		srand48 ( clock() ) ;
+		// *( matB + i ) = drand48() ;	
+		scanf( "%d" , &num )	;
+		*( matB + i ) = num ;	
+	}
+
+	for ( int i = 0 ; i < ndim - 1 ; i++ )
+	{
+		for ( int j = i + 1 ; j < ndim ; j++ )
+		{
+			if ( * ( matA + i * ndim + i ) == 0 )
+			{
+				break ;
+			}
+			p = * ( matA + j * ndim + i ) / * ( matA + i * ndim + i ) ;
+			for ( int k = i ; k < ndim ; k++ )
+			{
+				*( matA + j * ndim + k ) -= p * ( *( matA + i * ndim + k ) ) ;
+			}
+			*( matB + j ) -= p * ( *( matB + i ) ) ;
+		}
+	}
+
+	for ( int i = ndim - 1 ; i >= 0 ; i-- )
+	{
+		if ( * (matA + i * ndim + i) == 0 )
+		{
+			printf( "No Solution Exists");
+			break ;
+		}
+		x = *( matB + i ) / * (matA + i * ndim + i) ;
+		printf ( "Answer: %f \n" , x ) ; 
+		for ( int k = 0 ; k <= i ; k++ )
+		{
+			*( matB + k ) -= x * ( *( matA + k * ndim + i ) ) ; 
+		}
+	} 
+
+	for ( int i = 0 ; i < ndim ; i++ )
+	{
+		// Iterate through the columns of the Matrix A 
+		for ( int j = 0 ; j < ndim ; j++ )
+		{
+			printf ( "%f \n" , *( matA + i * ndim + j ) ) ;
+		}
+		printf ( "%f \n" , *( matB + i ) ) ;
 	}
 
     // Start high resolution clock timer
 	clock_gettime( CLOCK_MONOTONIC , &start );
-
-
 
 	clock_gettime( CLOCK_MONOTONIC , &end );	// End clock timer.
 	//Calculate the difference in timer and convert it to nanosecond by multiplying by 10^9
@@ -59,5 +108,6 @@ int main( int argc, char *argv[] )
 
 	//Deallocate the memory allocated to matrices A, B and C
 	free ( matA ) ;
+	free ( matB ) ;
 	exit( 0 ) ;
 }
